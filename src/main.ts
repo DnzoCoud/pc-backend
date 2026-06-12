7c989371f169893ce8f9b94be0b4101348feb9ba
Daniel Rodriguez <dangelsfocus@gmail.com>
2026-06-11 21:29:56 -0500

    [feat]: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+const config = new DocumentBuilder()
+  .setTitle('Interview API')
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 }
 bootstrap();
